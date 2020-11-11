a0877c08ca209a249be04725946dd7e5ce3969e2
FallenTurret <gavrilov_dan@mail.ru>
2020-11-12 01:53:23 +0300

    replaceTable method extracted

--- java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/database/ProductsDatabase.java
+++ java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/database/ProductsDatabase.java
@@ -9,30 +9,27 @@ public class ProductsDatabase {
     private static final String URL = "jdbc:sqlite:test.db";
 
     public static void createTableIfNotExists() throws SQLException {
-        try (Connection c = DriverManager.getConnection(URL)) {
-            String sql = "CREATE TABLE IF NOT EXISTS PRODUCT" +
-                    "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
-                    " NAME           TEXT    NOT NULL, " +
-                    " PRICE          INT     NOT NULL)";
-            Statement stmt = c.createStatement();
-
-            stmt.executeUpdate(sql);
-            stmt.close();
-        }
+        replaceTable("CREATE TABLE IF NOT EXISTS PRODUCT" +
+                "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                " NAME           TEXT    NOT NULL, " +
+                " PRICE          INT     NOT NULL)");
     }
 
     public static void dropTableIfExists() throws SQLException {
-        try (Connection c = DriverManager.getConnection(URL)) {
-            String sql = "DROP TABLE IF EXISTS PRODUCT";
-            Statement stmt = c.createStatement();
-
-            stmt.executeUpdate(sql);
-            stmt.close();
-        }
+        replaceTable("DROP TABLE IF EXISTS PRODUCT");
     }
 
     public static void createNewTable() throws SQLException {
         dropTableIfExists();
         createTableIfNotExists();
+    }
+
+    private static void replaceTable(String sql) throws SQLException {
+        try (Connection c = DriverManager.getConnection(URL)) {
+            Statement stmt = c.createStatement();
+
+            stmt.executeUpdate(sql);
+            stmt.close();
+        }
     }
 }
